53068f777244bcc1b3a8209cd64aaa928d18cd98
dakande85-bit <dakande85@gmail.com>
2026-06-10 16:40:15 +0100

    Route campaign to scroll film experience

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ScrollFilm        from './ScrollFilm.jsx';
-import Drop001Page       from './pages/Drop001.jsx';
-import ProductDetailPage from './pages/ProductDetailRoute.jsx';
-import ApparelPage       from './pages/Apparel.jsx';
-import FootwearPage      from './pages/Footwear.jsx';
-import EquipmentPage     from './pages/Equipment.jsx';
-import CampaignPage      from './pages/Campaign.jsx';
-import FightClubPage     from './pages/FightClub.jsx';
-import AdminAssetManager from './pages/AdminAssetManager.jsx';
+import ScrollFilm         from './ScrollFilm.jsx';
+import CampaignScrollFilm from './CampaignScrollFilm.jsx';
+import Drop001Page        from './pages/Drop001.jsx';
+import ProductDetailPage  from './pages/ProductDetailRoute.jsx';
+import ApparelPage        from './pages/Apparel.jsx';
+import FootwearPage       from './pages/Footwear.jsx';
+import EquipmentPage      from './pages/Equipment.jsx';
+import FightClubPage      from './pages/FightClub.jsx';
+import AdminAssetManager  from './pages/AdminAssetManager.jsx';
 
 export default function AppRouter() {
   return (
@@ -19,8 +19,8 @@ export default function AppRouter() {
         <Route path="/apparel"       element={<ApparelPage />} />
         <Route path="/footwear"      element={<FootwearPage />} />
         <Route path="/equipment"     element={<EquipmentPage />} />
-        <Route path="/campaign"      element={<CampaignPage />} />
-        <Route path="/the-campaign"  element={<CampaignPage />} />
+        <Route path="/campaign"      element={<CampaignScrollFilm />} />
+        <Route path="/the-campaign"  element={<CampaignScrollFilm />} />
         <Route path="/fight-club"    element={<FightClubPage />} />
         <Route path="/fightclub"     element={<FightClubPage />} />
         <Route path="/admin"         element={<AdminAssetManager />} />
