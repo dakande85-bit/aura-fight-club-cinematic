0490bcecd201dd574aec19a8b6aba113bc211e69
dakande85-bit <dakande85@gmail.com>
2026-06-10 21:39:28 +0100

    Load scroll film mobile fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import AppRouter from './AppRouter.jsx';
 import './styles/global.css';
 import './styles/scroll-film.css';
+import './styles/scroll-film-fixes.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
