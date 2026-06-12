c0173b251d9a6d1a21845db9c7a16d896eef787f
dakande85-bit <dakande85@gmail.com>
2026-06-12 17:40:04 +0100

    wire campaign page footer wrapper

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,35 +1,34 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage           from './pages/HomePage.jsx';
-import CampaignScrollFilm from './CampaignScrollFilm.jsx';
-import Drop001Page        from './pages/Drop001.jsx';
-import ProductDetailPage  from './pages/ProductDetailRoute.jsx';
-import ApparelPage        from './pages/Apparel.jsx';
-import FootwearPage       from './pages/Footwear.jsx';
-import EquipmentPage      from './pages/Equipment.jsx';
-import FightClubPage      from './pages/FightClub.jsx';
-import AdminAssetManager  from './pages/AdminAssetManager.jsx';
+import HomePage from './pages/HomePage.jsx';
+import CampaignPage from './pages/CampaignPage.jsx';
+import Drop001Page from './pages/Drop001.jsx';
+import ProductDetailPage from './pages/ProductDetailRoute.jsx';
+import ApparelPage from './pages/Apparel.jsx';
+import FootwearPage from './pages/Footwear.jsx';
+import EquipmentPage from './pages/Equipment.jsx';
+import FightClubPage from './pages/FightClub.jsx';
+import AdminAssetManager from './pages/AdminAssetManager.jsx';
 import CinematicSceneBuilder from './pages/admin/CinematicSceneBuilder.jsx';
-import AdminSuppliers     from './pages/AdminSuppliers.jsx';
+import AdminSuppliers from './pages/AdminSuppliers.jsx';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"              element={<HomePage />} />
-        <Route path="/drop-001"      element={<Drop001Page />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/drop-001" element={<Drop001Page />} />
         <Route path="/product/:slug" element={<ProductDetailPage />} />
-        <Route path="/apparel"       element={<ApparelPage />} />
-        <Route path="/footwear"      element={<FootwearPage />} />
-        <Route path="/equipment"     element={<EquipmentPage />} />
-        <Route path="/campaign"      element={<CampaignScrollFilm />} />
-        <Route path="/the-campaign"  element={<CampaignScrollFilm />} />
-        <Route path="/fight-club"    element={<FightClubPage />} />
-        <Route path="/fightclub"     element={<FightClubPage />} />
-        <Route path="/admin"         element={<AdminAssetManager />} />
+        <Route path="/apparel" element={<ApparelPage />} />
+        <Route path="/footwear" element={<FootwearPage />} />
+        <Route path="/equipment" element={<EquipmentPage />} />
+        <Route path="/campaign" element={<CampaignPage />} />
+        <Route path="/the-campaign" element={<CampaignPage />} />
+        <Route path="/fight-club" element={<FightClubPage />} />
+        <Route path="/fightclub" element={<FightClubPage />} />
+        <Route path="/admin" element={<AdminAssetManager />} />
         <Route path="/admin/cinematic" element={<CinematicSceneBuilder />} />
         <Route path="/admin/suppliers" element={<AdminSuppliers />} />
-        {/* Catch-all → homepage */}
-        <Route path="*"              element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
