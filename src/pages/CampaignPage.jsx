79b59a59225bbdb704337aab5460b69818f73273
dakande85-bit <dakande85@gmail.com>
2026-06-12 17:39:14 +0100

    add campaign page wrapper with footer

---
+++ src/pages/CampaignPage.jsx
@@ -0,0 +1,11 @@
+import CampaignScrollFilm from '../CampaignScrollFilm.jsx';
+import Footer from '../components/Footer.jsx';
+
+export default function CampaignPage() {
+  return (
+    <>
+      <CampaignScrollFilm />
+      <Footer />
+    </>
+  );
+}
