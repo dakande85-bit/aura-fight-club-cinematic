df57ea22e750f6846df43bb116cfcfe068db11f7
dakande85-bit <dakande85@gmail.com>
2026-06-12 17:37:24 +0100

    add homepage wrapper with commercial sections

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import ScrollFilm from '../ScrollFilm.jsx';
+import HomepageExtras from '../components/HomepageExtras.jsx';
+import Footer from '../components/Footer.jsx';
+
+export default function HomePage() {
+  return (
+    <>
+      <ScrollFilm />
+      <HomepageExtras />
+      <Footer />
+    </>
+  );
+}
