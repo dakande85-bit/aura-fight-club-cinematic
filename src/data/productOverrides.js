2ea0e7961f33894ffecd98d8ebd328e73cd3f91a
dakande85-bit <dakande85@gmail.com>
2026-07-14 19:52:52 +0100

    Replace Essential Tee front product image

--- src/data/productOverrides.js
+++ src/data/productOverrides.js
@@ -1,5 +1,5 @@
 const ESSENTIAL_TEE_MEDIA = {
-  front: 'https://cdn.shopify.com/s/files/1/1029/3339/7846/files/aura-boxing-essential-navy-front_df74721f-1951-4512-8355-c82b62e770ea.png?v=1784052972',
+  front: 'https://cdn.shopify.com/s/files/1/1029/3339/7846/files/aura-boxing-essential-navy-front-v2_071fa5a7-76ef-482c-a14b-91590bdc6397.png?v=1784055124',
   model: 'https://cdn.shopify.com/s/files/1/1029/3339/7846/files/aura-boxing-essential-navy-model-hover_dc7a6fe0-bef6-4ebd-acb2-b044db599065.png?v=1784052972',
   back: 'https://cdn.shopify.com/s/files/1/1029/3339/7846/files/aura-boxing-essential-navy-back_f3ecc250-ce86-47b4-8584-16f7b52583a3.png?v=1784052972',
   detail: 'https://cdn.shopify.com/s/files/1/1029/3339/7846/files/aura-boxing-essential-navy-logo-detail_f1c8db0b-8904-455c-9cd4-250b567c0af9.png?v=1784052972',
@@ -20,7 +20,7 @@ const PRODUCT_OVERRIDES = {
     primaryImage: ESSENTIAL_TEE_MEDIA.front,
     image: ESSENTIAL_TEE_MEDIA.front,
     hoverImage: ESSENTIAL_TEE_MEDIA.model,
-    imageAlt: 'AURA Boxing Essential Unisex T-Shirt in navy, front view',
+    imageAlt: 'AURA Boxing Essential Unisex T-Shirt in navy with gold chest emblem, front view',
     galleryImages: [
       ESSENTIAL_TEE_MEDIA.front,
       ESSENTIAL_TEE_MEDIA.model,
@@ -29,7 +29,7 @@ const PRODUCT_OVERRIDES = {
       ESSENTIAL_TEE_MEDIA.lifestyle,
     ],
     gallery: [
-      { src: ESSENTIAL_TEE_MEDIA.front, alt: 'AURA Boxing Essential Unisex T-Shirt in navy, front view' },
+      { src: ESSENTIAL_TEE_MEDIA.front, alt: 'AURA Boxing Essential Unisex T-Shirt in navy with gold chest emblem, front view' },
       { src: ESSENTIAL_TEE_MEDIA.model, alt: 'Male model wearing the AURA Boxing Essential navy unisex T-shirt' },
       { src: ESSENTIAL_TEE_MEDIA.back, alt: 'AURA Boxing Essential Unisex T-Shirt in navy, back view' },
       { src: ESSENTIAL_TEE_MEDIA.detail, alt: 'Close-up detail of the gold AURA emblem on the navy T-shirt' },
